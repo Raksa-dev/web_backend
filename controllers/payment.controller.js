fdce437aa64ece4b42110ff6a5b02be51b9d0548
moses12345 <shaikmoses1997@gmail.com>
2023-09-15 19:42:59 +0530

    new config

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -3,24 +3,20 @@ const nodeCCAvenue = require("node-ccavenue");
 class PaymentController {
   static async handlePaymentController(req, res, next) {
     try {
-      const merchant_id = "2711780";
-      const test_working_key = "02182BD7D50FAF0553F00BC70A412F8C";
-      const access_code = "AVIA09KI06AB67AIBA";
       const ccav = new nodeCCAvenue.Configure({
-        merchant_id: merchant_id,
-        working_key: test_working_key,
+        ...req.body.keys,
+        merchant_id: "2711780",
       });
       const orderParams = {
-        merchant_id: merchant_id,
         redirect_url: encodeURIComponent(`https://raksa.tech/api/response`),
         billing_name: "Name of the customer",
-        ...req.body,
+        ...req.body.orderParams,
       };
       const encryptedOrderData = ccav.getEncryptedOrder(orderParams);
       let formbody =
         '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
         encryptedOrderData +
-        `"><input type="hidden" name="access_code" id="access_code" value='${access_code}'><script language="javascript">document.redirect.submit();</script></form>`;
+        `"><input type="hidden" name="access_code" id="access_code" value='${req.body.keys.access_code}'><script language="javascript">document.redirect.submit();</script></form>`;
 
       res.setHeader("content-type", "text/html");
       res.status(200).send(formbody);
